96c1740d19d6b417a6044b9c59d4838e3eefd112
edwin <edwin@homerlogistics.com>
2018-01-04 13:32:30 -0500

    Revert to previous start application. Env variables are not available

--- lib/ex_magic_eightball_webapp.ex
+++ lib/ex_magic_eightball_webapp.ex
@@ -11,8 +11,7 @@ defmodule ExMagicEightballWebapp do
   Init function that runs on application start.
   """
   def start(_type, _args) do
-    IO.puts("STARTING")
-
+    
     opts = [strategy: :one_for_one]
     Supervisor.start_link(children(), opts)
   end
@@ -22,13 +21,9 @@ defmodule ExMagicEightballWebapp do
   not enable the router for Mix.env == :test
   """
   def children() do
-    serve_http = Application.get_env(:ex_magic_eightball_webapp, :serve_http, false)
-    IO.puts("applicaiton available?")
-    IO.puts(serve_http)
-
-    case serve_http do
-      false -> []
-      true -> [
+    case Mix.env do
+      :test -> []
+      _ -> [
         worker(ExMagicEightballWebapp.Router, [])
       ]
     end
