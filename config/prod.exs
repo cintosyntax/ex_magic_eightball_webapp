8c10600c21de7e6a11e6afce0d6afe8d44e8697d
edwin <edwin@homerlogistics.com>
2018-01-04 13:38:56 -0500

    Hardcode port entry to 80

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,4 @@
 use Mix.Config
 
-port = case System.get_env("PORT") do
-  port when is_binary(port) -> String.to_integer(port)
-  nil -> 80 # default port
-end
-
 config :ex_magic_eightball_webapp, 
-  port: port
+  port: 80
